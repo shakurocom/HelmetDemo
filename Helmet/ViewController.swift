4c414d8b0698a39a926eec6fd15a0a9c308410c5
EugeneKlyuenkov <dawd>
2022-06-23 11:28:15 +0300

    Initial Commit

---
+++ Helmet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Helmet
+//
+//  Created by Eugene Klyuenkov on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
